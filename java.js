0c4d8c58e7a48bdc2f075e93a6d5789855fffb78
tinchoo290 <perez.martinmgl@gmail.com>
2021-12-08 22:34:09 +0100

    pre entrega 2

--- java.js
+++ java.js
@@ -1,102 +1,113 @@
-class Producto {
-    constructor(nombre , cantidad , precio ) {
-        this.nombre = nombre;  
-        this.cantidad = cantidad;  
-        this.precio = parseFloat (precio);
-        this.disponible = true; 
-    }
-
-    precioFinal() {
-        let iva = this.precio * 0.21
-        return this.precio + iva
-    }
-    vender () {
-        this.disponible = false;
-    }
-    precioSugerido () {
-        return this.precio * 1.21 * 1.25;
+// ARRAYS DE PRODUCTOS
+const productos = {
+    producto1: {
+      nombre: 'Chicken Burger',
+      precio: '3.50',
+      descripcion: 'medallón de pollo super sabrosa con Papas frescas preparadas en el momento',
+      srcImg: 'img/burguer1.jpg'
+    },
+    producto2: {
+      nombre: 'Beef Burger',
+      precio: '5.00',
+      descripcion: 'La mejor hamburguesa de carne vacuna de selección, lechuga, tomate y queso chedar.',
+      srcImg: 'img/burguer2.jpg'
+    },
+    producto3: {
+      nombre: 'Pork Burger',
+      precio: '4.50',
+      descripcion: 'Masa preparada en el local, medallón de cerdo sumamente sazonado, UNA CARICIA AL PALADAR.',
+      srcImg: 'img/burguer3.jpg'
+    },
+    producto4: {
+      nombre: 'Vegan Burger',
+      precio: '7.50',
+      descripcion: 'Burger Vegana con extra verduras.. un sabor inigualable e inmejorable.',
+      srcImg: 'img/burguer4.jpg'
     }
 }
 
-//Variables globales
-
-
-let botonesCompras = document.querySelectorAll(".botonCompra");
-let arrayProductos = [];
-
-
-
-for( let boton of botonesCompras){
-
-    boton.addEventListener("click" , agregarCarrito);
-
-}
-
-console.log(botonesCompras);
-
-function agregarCarrito (e) {
-
-
-    console.log(e.target);
-    let prod = e.target;
-    let produ = prod.parentNode.parentNode;
-    let titulo = produ.querySelector("h5").textContent;
-    let parrafo = produ.querySelector("p").textContent;
-    let imagen = produ.querySelector("img").src;
-
-    let productos = {
-        nombre: titulo,
-        texto: parrafo,
-        img: imagen,
-    };
-
-    arrayProductos.push(productos);
-    console.log(produ);
-    console.log(titulo);
-
-
-    for (let Producto of arrayProductos) {
-        let contenedor = document.createElement("p");
-        //Inner
-        contenedor.innerHTML = `<h5> Nombre: ${Producto.nombre}</h5>
-                                <p> Precio: ${Producto.precio} </p>
-                                <p> Cantidad: ${Producto.cantidad} </p>`;
-   
-        document.body.appendChild (contenedor);
-    }
-
-
-}
-
-/* let arrayProductos = [];
-let product = 0 ; 
-
-do {
-    let product = prompt ("Ingrese el producto que desea comprar o fin para terminar de agregar"); 
-    if (product === "fin" || product === "FIN" || product ==="Fin"){
-        break;
-    }
-    else {
-        const nombreP = product;
-        const precioP = prompt ("Ingrese el precio del producto");
-        const cantidadP = prompt ("Ingrese la cantidad comprada del producto");
-        arrayProductos.push(new Producto(nombreP, precioP, cantidadP ));
-    }
+  // TEMPLATE DE LOS PRODUCTOS
+  const templateProd = document.getElementById('template-prod').content
+  const contenedorProd = document.querySelector('.contenedor-productos')
+  const fragment = document.createDocumentFragment()
+  
+  
+  // AGREGAR LOS PRODUCTOS
+  Object.values(productos).forEach( producto => {
+    templateProd.querySelector('.div-info .nombre-prod').textContent = producto.nombre
+    templateProd.querySelector('.div-precio-boton .precio').textContent = producto.precio
+    templateProd.querySelector('.div-info .descripcion-prod').textContent = producto.descripcion
+    templateProd.querySelector('.contenedor-img img').setAttribute('alt', producto.nombre)
+    templateProd.querySelector('.contenedor-img img').setAttribute('src', producto.srcImg)
+    const clone = templateProd.cloneNode(true)
+    fragment.appendChild(clone)
+  })
+  contenedorProd.appendChild(fragment)
+  
+  // CARRITO DE COMPRA
+  let carrito = {}
+  const templateTabla = document.getElementById('agregar-producto-al-carro').content
+  const tbodyCarrito = document.getElementById('carrito-body')
+  const fragmentTabla = document.createDocumentFragment()
+  const templateFoot = document.getElementById('tfooter').content
+  const tfootCarrito = document.getElementById('footer')
+  
+  contenedorProd.addEventListener('click', e => {
     
-} 
+    if(e.target.textContent === "Agregar") {
+      setCarrito(e.target.parentElement.parentElement)
+    }
+    e.stopPropagation();
+  })
+  const setCarrito = e => {
+    const pivoteCarrito = {
+      nombre: e.querySelector('.div-info .nombre-prod').textContent,
+      precio: e.querySelector('.div-precio-boton .precio').textContent,
+      cantidad: 1
+    }
+    if(carrito.hasOwnProperty(pivoteCarrito.nombre)){
+      carrito[pivoteCarrito.nombre].cantidad += 1
+    } else {
+      carrito[pivoteCarrito.nombre] = {...pivoteCarrito}
+    }
+    pintarTabla(carrito)
+  }
+  
+  const pintarTabla = objetoCarrito => {
+    Object.values(objetoCarrito).forEach( objeto => {
+      const cloneTabla = templateTabla.cloneNode(true)
+      cloneTabla.getElementById('producto').textContent = objeto.nombre
+      cloneTabla.getElementById('cant').textContent = objeto.cantidad
+      cloneTabla.getElementById('precio-uni').textContent = objeto.precio
+      let precioTotal = parseFloat(objeto.precio) * objeto.cantidad
+      cloneTabla.getElementById('precio-total-prod').textContent = precioTotal.toFixed(2)
+      fragmentTabla.appendChild(cloneTabla)
+    })
+    tbodyCarrito.innerHTML = ''
+    tbodyCarrito.appendChild(fragmentTabla)
+    pintarFooter()
+  }
+  const pintarFooter = () => {
+    tfootCarrito.innerHTML = ''
+    if(Object.keys(carrito).length === 0) {
+      tfootCarrito.innerHTML = '<tr><td colspan = 4>¡No hay ningún elemento en el carrito!</td></tr>'
+    } else {
+      const total = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + (cantidad * precio),0)
+      templateFoot.getElementById('total-a-pagar').textContent = total.toFixed(2)
+      const cloneFoot = templateFoot.cloneNode(true)
+      fragment.appendChild(cloneFoot)
+      tfootCarrito.appendChild(fragment)
+      //Boton Vaciar carrito
+      const botonVaciar = document.getElementById('vaciar-tabla')
+  botonVaciar.addEventListener('click', () => {
+        carrito = {}
+        pintarTabla(carrito)
+        pintarFooter()
+      })
+      
 
-while (product != "fin" || product != "Fin" || product != "FIN" );
+      
+    }
+  }
 
-console.log (arrayProductos);
-
-for (let Producto of arrayProductos) {
-    let contenedor = document.createElement("p"); 
-    //Inner
-    contenedor.innerHTML = `<h5> Nombre: ${Producto.nombre}</h5>
-                            <p> Precio: ${Producto.precio} </p>
-                            <p> Cantidad: ${Producto.cantidad} </p>`;
-
-    document.body.appendChild (contenedor); 
-}
-
- */
+  
